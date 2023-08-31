8560d084e6350a5470daf6374a2511cd6dafe9e5
Santiago <tiago.san44@gmail.com>
2023-08-31 16:39:54 -0500

    UC#7 Infrastructure setup

---
+++ src/test/java/com/softserve/sschatgpt/SsChatgptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softserve.sschatgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsChatgptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
